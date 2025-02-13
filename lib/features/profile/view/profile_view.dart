ccb0f1a6d8ae5ce0a9a0087699e7d8d5c1d04070
yousefelsayed <eyousef464@gmail.com>
2025-02-13 15:43:46 +0200

    Implement CustomAppBar in ProfileView and update Body widget

--- lib/features/profile/view/profile_view.dart
+++ lib/features/profile/view/profile_view.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:team1_khayat/core/utils/app_assets.dart';
 import 'package:team1_khayat/features/profile/view/widget/body.dart';
+import 'package:team1_khayat/shared/my_appbar/custom_app_bar.dart';
 
 class ProfileView extends StatelessWidget {
   const ProfileView({super.key});
@@ -7,7 +11,22 @@ class ProfileView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Body(),
+        backgroundColor: Colors.black,
+          appBar: _buildAppBar(context),
+      body: const Body(),
+    );
+  }
+   PreferredSizeWidget _buildAppBar(BuildContext context) {
+    return CustomAppBar(
+      leadingWidth: 69.h,
+      actions: [
+        IconButton(
+          icon: SvgPicture.asset(
+            AppAssets.iconsSearch,
+          ),
+          onPressed: () {},
+        ),
+      ],
     );
   }
 }
