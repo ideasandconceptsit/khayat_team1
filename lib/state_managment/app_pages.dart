0c7480b0da491b08f1f316ed085eac559be07505
amr <amr232948@gmail.com>
2025-02-23 20:59:04 +0200

    Favourit

--- lib/state_managment/app_pages.dart
+++ lib/state_managment/app_pages.dart
@@ -1,41 +1,13 @@
 import 'package:get/get.dart';
-import 'package:team1_khayat/features/Catalog2/catalog.dart';
+
 import 'package:team1_khayat/features/Favourite/favourite_home.dart';
 import 'package:team1_khayat/features/Favourite/homeaddfavorites/addfavorites.dart';
-import 'package:team1_khayat/features/authintication/view/signup_pages/signup_page.dart';
-import 'package:team1_khayat/features/authintication/view/verification_pages/verification_page.dart';
-import 'package:team1_khayat/features/cart/views/cart_view.dart';
+
 import 'package:team1_khayat/state_managment/app_routers.dart';
-import '../features/authintication/view/login_pages/login_page.dart';
-import '../features/homepage/view/homepage.dart';
-import '../features/todo/view/todo_page.dart';
+
 
 class AppPages {
   static final pages = [
-    GetPage(
-      name: Routes.signupPage,
-      page: () => SignupPage(),
-    ),
-    GetPage(
-      name: Routes.loginPage,
-      page: () => LoginPage(),
-    ),
-    GetPage(
-      name: Routes.verificationPage,
-      page: () => const VerificationPage(),
-    ),
-    GetPage(
-      name: Routes.homePage,
-      page: () => Homepage(),
-    ),
-    GetPage(
-      name: Routes.catalog_2Page,
-      page: () => const CatalogPage(),
-    ),
-    GetPage(
-      name: Routes.cartPage,
-      page: () => CartView(),
-    ),
     GetPage(
       name: Routes.favouritehome,
       page: () => FavouriteHome(),
